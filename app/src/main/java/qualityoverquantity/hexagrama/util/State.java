3f1d1f9d6f3447f77856cafc9bbbf01d26ae6820
uo251851 <marcialfco@live.com>
2018-12-02 15:19:38 +0100

    Merge.

---
+++ app/src/main/java/qualityoverquantity/hexagrama/util/State.java
@@ -0,0 +1,17 @@
+package qualityoverquantity.hexagrama.util;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by MarcialFrancisco on 01/12/2018.
+ */
+
+public final class State {
+
+    public static boolean INSTRUCCIONES_INICIO = true;
+    public static boolean NARRADOR_INTERFAZ = true;
+    public static String TIPO_SALIDA="musical";
+    public static final String NARRADOR = "narrador";
+    public static final String MUSICAL = "musical";
+
+}
